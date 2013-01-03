0d37025d7898490d6d7d834a20b8f8db961db602
euf <euf@ornl.gov>
2013-01-03 11:30:26 -0500

    changes to how words and sentences are split, changes to how numbers are scored.

--- rake.py
+++ rake.py
@@ -8,6 +8,16 @@ import re
 import operator
 import math
 
+debug = False
+test = True
+
+def isnum (s):
+    try:
+    	float(s) if '.' in s else int(s)
+        return True
+    except ValueError:
+        return False
+
 # Utility function to load stop words from a file and return as a list of words
 # @param stopWordFile Path and file name of a file containing stop words.
 # @return list A list of stop words.
@@ -23,18 +33,24 @@ def loadStopWords(stopWordFile):
 # @param text The text that must be split in to words.
 # @param minWordReturnSize The minimum no of characters a word must have to be included.
 def separatewords(text,minWordReturnSize):
-	splitter=re.compile('[^a-zA-Z0-9_\\+\\-]')
-	return [singleWord.lower() for singleWord in splitter.split(text) if len(singleWord)>minWordReturnSize]
+	splitter=re.compile('[^a-zA-Z0-9_\\+\\-/]')
+	words = []
+	for singleWord in splitter.split(text):
+		currWord = singleWord.strip().lower()
+		#leave numbers in phrase, but don't count as words, since they tend to invlate scores of their phrases
+		if len(currWord)>minWordReturnSize and currWord != '' and not isnum(currWord): 
+			words.append(currWord)
+	return words
 
 # Utility function to return a list of sentences.
 # @param text The text that must be split in to sentences.
 def splitSentences(text):
-	sentenceDelimiters = re.compile('[.!?,;]')
+	sentenceDelimiters = re.compile(u'[.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
 	sentenceList = sentenceDelimiters.split(text)
 	return sentenceList
 
 def buildStopwordRegExPattern(pathtostopwordsfile):
-	stopwordlist = loadStopWords(stoppath)
+	stopwordlist = loadStopWords(pathtostopwordsfile)
 	stopwordregexlist = []
 	for wrd in stopwordlist:
 		wrdregex = '\\b' + wrd + '\\b'
@@ -42,7 +58,7 @@ def buildStopwordRegExPattern(pathtostopwordsfile):
 	stopwordpattern = re.compile('|'.join(stopwordregexlist), re.IGNORECASE)
 	return stopwordpattern
 
-def generateCandidateKeywords(stopwordpattern):
+def generateCandidateKeywords(sentenceList, stopwordpattern):
 	phraseList = []
 	for s in sentenceList:
 		tmp = re.sub(stopwordpattern, '|', s.strip())
@@ -60,11 +76,13 @@ def calculateWordScores(phraseList):
 		wordlist = separatewords(phrase,0) 
 		wordlistlength = len(wordlist)
 		wordlistdegree = wordlistlength - 1
+		#if wordlistdegree > 3: wordlistdegree = 3 #exp.
 		for word in wordlist:
 			wordfreq.setdefault(word,0)
 			wordfreq[word] += 1
 			worddegree.setdefault(word,0)
-			worddegree[word] += wordlistdegree
+			worddegree[word] += wordlistdegree #orig.
+			#worddegree[word] += 1/(wordlistlength*1.0) #exp.
 	for item in wordfreq:
 		worddegree[item] = worddegree[item]+wordfreq[item] 	
 
@@ -72,7 +90,8 @@ def calculateWordScores(phraseList):
 	wordscore = {}
 	for item in wordfreq:
 		wordscore.setdefault(item,0)
-		wordscore[item] = worddegree[item]/(wordfreq[item] * 1.0)
+		wordscore[item] = worddegree[item]/(wordfreq[item] * 1.0) #orig.
+		#wordscore[item] = wordfreq[item]/(worddegree[item] * 1.0) #exp.
 	return wordscore
 	
 def generateCandidateKeywordScores(phraseList, wordscore):
@@ -86,27 +105,28 @@ def generateCandidateKeywordScores(phraseList, wordscore):
 		keywordcandidates[phrase] = candidatescore
 	return keywordcandidates
 
-text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types."
+if test:
+	text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types."
 
-# Split text into sentences
-sentenceList = splitSentences(text)
-#stoppath = "FoxStoplist.txt" #Fox stoplist contains "numbers", so it will not find "natural numbers" like in Table 1.1
-stoppath = "SmartStoplist.txt" #SMART stoplist misses some of the lower-scoring keywords in Figure 1.5, which means that the top 1/3 cuts off one of the 4.0 score words in Table 1.1
-stopwordpattern = buildStopwordRegExPattern(stoppath)
+	# Split text into sentences
+	sentenceList = splitSentences(text)
+	#stoppath = "FoxStoplist.txt" #Fox stoplist contains "numbers", so it will not find "natural numbers" like in Table 1.1
+	stoppath = "SmartStoplist.txt" #SMART stoplist misses some of the lower-scoring keywords in Figure 1.5, which means that the top 1/3 cuts off one of the 4.0 score words in Table 1.1
+	stopwordpattern = buildStopwordRegExPattern(stoppath)
 
-# generate candidate keywords
-phraseList = generateCandidateKeywords(stopwordpattern)
+	# generate candidate keywords
+	phraseList = generateCandidateKeywords(sentenceList, stopwordpattern)
 
-# calculate individual word scores
-wordscores = calculateWordScores(phraseList)
+	# calculate individual word scores
+	wordscores = calculateWordScores(phraseList)
 
-# generate candidate keyword scores
-keywordcandidates = generateCandidateKeywordScores(phraseList, wordscores)
-#print keywordcandidates
+	# generate candidate keyword scores
+	keywordcandidates = generateCandidateKeywordScores(phraseList, wordscores)
+	if debug: print keywordcandidates
 
-sortedKeywords = sorted(keywordcandidates.iteritems(), key=operator.itemgetter(1), reverse=True)
-#print sortedKeywords
+	sortedKeywords = sorted(keywordcandidates.iteritems(), key=operator.itemgetter(1), reverse=True)
+	if debug: print sortedKeywords
 
-totalKeywords = len(sortedKeywords)
-#print totalKeywords
-print sortedKeywords[0:(totalKeywords/3)]
+	totalKeywords = len(sortedKeywords)
+	if debug: print totalKeywords
+	print sortedKeywords[0:(totalKeywords/3)]
