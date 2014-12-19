c9bbb262e8eb45635de08c349c1ddb814d511f4b
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-12-19 15:03:32 +0800

    fixed hyphen

--- rake.py
+++ rake.py
@@ -54,7 +54,7 @@ def split_sentences(text):
     Utility function to return a list of sentences.
     @param text The text that must be split in to sentences.
     """
-    sentence_delimiters = re.compile(u'[.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
+    sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\s\-\s')
     sentences = sentence_delimiters.split(text)
     return sentences
 
